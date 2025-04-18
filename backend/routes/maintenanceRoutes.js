c71286f708b887a5e4b3cdc352be08b9917100f7
smiggy10 <ralphjan341@gmail.com>
2025-04-18 17:06:09 +0800

    Integration of Data and Securit

--- backend/routes/maintenanceRoutes.js
+++ backend/routes/maintenanceRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middleware/authenticate');
 const maintenanceController = require('../controllers/maintenanceController');
 
 // Create request
